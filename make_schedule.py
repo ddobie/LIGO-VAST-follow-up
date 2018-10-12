ecb3a63bfb1780d29cbd361bf808dc14729651ce
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2018-10-12 16:06:16 +1100

    Added function to do complete event response

--- make_schedule.py
+++ make_schedule.py
@@ -8,12 +8,6 @@ import cabb_scheduler as cabb
 import os
 
 
-#from mirexec import TaskBase
-
-#class TaskATMOS (TaskBase):
-#  _keywords = ['source', 'out', 'cycles', 'interval', 'origin', 'ref', 'lst']
-#  _options =  ['fixed']
-
 def load_targets(filename = 'example_targets.dat'):
   '''
   Load a list of candidate host galaxies from the CLU catalogue.
@@ -82,6 +76,12 @@ def shorten_name(name):
   return short_name[:8]
   
 def output_file(target_list, outfile):
+  '''
+  Make a temporary file from a list of targets to input to atmos
+  
+  :param target_list: An astropy table, the name, RA and Dec of the targets
+  :param outfile: File to output to
+  '''
   ascii.write(target_list,outfile, format='no_header')
   return
 
@@ -102,12 +102,6 @@ def make_mosfile(target_list, mosfile, cycles=6, tempfile='temp_list.dat'):
   
   os.system("atmos source='%s' out='%s' cycles=%d"%(tempfile, mosfile, cycles))
   
-  
-  '''
-  Need to work out some way to include calibrators in the mosaic. Get them automatically using Jamie's cabb python module? Or split the mosaic into parts? Do we need more than one phase calibrator?
-  
-  '''
-  
   return
   
 def choose_phase_calibrator(RA, Dec, freq1=5500, freq2=9000, project='C3278'):
@@ -123,21 +117,69 @@ def choose_phase_calibrator(RA, Dec, freq1=5500, freq2=9000, project='C3278'):
   
   print("Calibrator chosen: %s, %.1f degrees away" % (bestCal['calibrator'].getName(),
                                                     bestCal['distance']))
+def extract_ref_pos(mosfile):
+  '''
+  Extract the reference position for a mosaic file output by atmos.
+  
+  :param mosfile: mosaic file name
+  '''
+  f = open(mosfile,'rU')
+  lines = f.readlines()
+  f.close()
+  ref_line = lines[1].strip()
+  
+  coord = ref_line.split(' = ')[1]
+  ra, dec = coord.split(' ')
+  
+  return ra, dec
   
 
- 
+def make_sched_file(mosfile, schedfile, freq1=5500, freq2=9000, project='C3278', cal_scan_length='00:02:00', observer='DDobie'):
+  '''
+  Make an ATCA schedule file to observe a mosaic
+  
+  :param mosfile: mosaic filename
+  :param schedfile: sched filename
+  :param freq1: central frequency of first band
+  :param freq1: central frequency of second band
+  :param project: project code
+  :param cal_scan_length: length of scan on phase calibrator
+  
+  '''
+
+
+  ref_ra, ref_dec = extract_ref_pos(mosfile)
+  
+  schedule = cabb.schedule()
+  
+  mos_scan = schedule.addScan(
+    { 'source': mosfile.split('.')[0], 'rightAscension': ref_ra, 'declination': ref_dec,
+      'freq1': freq1, 'freq2': freq2, 'project': project, 'scanLength': '00:01:00', 'scanType': 'Mosaic', 'observer': observer}) #set scanLength to 1 minute, because the mosaic will loop at least once
+      
+      
+      
+  calList = mos_scan.findCalibrator()
+
+  bestCal = calList.getBestCalibrator()
+  
+  calScan = schedule.addCalibrator(bestCal['calibrator'], mos_scan, { 'scanLength': cal_scan_length, 'scanType': 'Dwell'}) #need to change the scanType to Dwell, or it uses same as previous scan (mosaic)
+  
+  schedule.setLooping(False)
+  
+  schedule.write(name=schedfile)
+  
+  
+def event_response(event_code):
+  galaxy_list_fname = '%s_galaxies.dat'%(event_code)
+  mos_fname = '%s.mos'%(event_code)
+  sched_fname = '%s.sch'%(event_code)
+  
+
+  target_list, name_dict = load_targets(filename=galaxy_list_fname)
+  
+  make_mosfile(target_list, mos_fname)
+  
+  make_sched_file(mos_fname, sched_fname)
 
 if __name__ == '__main__':
-  target_list, name_dict = load_targets()
-  
-  print(target_list[0])
-  
-  choose_phase_calibrator(target_list[0]['ra'], target_list[0]['dec'])
-
-
-
-  make_mosfile(target_list, 'mosaicfile.mos')
-  
-  
-  
-  #print(target_list)
+  event_response('G298048')
