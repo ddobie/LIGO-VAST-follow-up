ab8c21307b429d817f07ee98867bb562fdeaf790
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2019-03-11 16:34:55 +1100

    added some documentation (still need more!)

--- vastligo/clustering.py
+++ vastligo/clustering.py
@@ -3,11 +3,9 @@ from astropy.table import Table
 from astropy.coordinates import SkyCoord, cartesian_to_spherical
 from astropy import units as u
 import numpy as np
-from string import ascii_lowercase
 import cabb_scheduler as cabb
 import os
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import time
 
 import scipy.spatial.distance as ssd
@@ -16,6 +14,12 @@ from scipy.cluster.hierarchy import dendrogram, linkage, fcluster, cut_tree
 import make_schedule
 
 def calc_separation(coords):
+  '''
+  Calculate the separation between every combination of targets
+  :param coords: an astropy SkyCoord containing the coordinates of the target catalogue
+  
+  '''
+  print(type(coords))
   seps = np.empty((len(coords),len(coords)))
 
   for i,coord in enumerate(coords):
@@ -26,6 +30,11 @@ def calc_separation(coords):
   return seps
 
 def load_targets(filename='targets.dat'):
+  '''
+  Load targets for testing
+  :param filename: a string, the name of file containing the target catalogue
+  
+  '''
   targets = ascii.read(filename)
   targets = targets[:-1]
   
@@ -58,6 +67,13 @@ def beam_fit(x, fwhm=48.3, nu=5.5):
 
 
 def extract_levels(row_clusters, labels):
+  '''
+  Decondense the linkage matrix and find which targets are in which cluster. Function taken from here: https://stackoverflow.com/questions/7664826/how-to-get-flat-clustering-corresponding-to-color-clusters-in-the-dendrogram-cre/43170608#43170608
+  
+  :param row_clusters: a numpy array containing the linkage matrix
+  :param labels: a numpy array containing the name of each target
+  
+  '''
   clusters = {}
   for row in range(row_clusters.shape[0]):
     cluster_n = row + len(labels)
@@ -77,6 +93,12 @@ def extract_levels(row_clusters, labels):
   return clusters
     
 def calc_centroid(coords):
+  '''
+  Calculate the centroid of a series of coordinates
+  
+  :param coords: an astropy SkyCoord containing the coordinates
+  
+  '''
   ccoords = coords.cartesian
   centroid = np.sum(ccoords)
   
@@ -84,6 +106,12 @@ def calc_centroid(coords):
 
   
 def calc_circumcentre(coords):
+  '''
+  Calculate the circumcentre of 3 coordinates
+  
+  :param coords: an astropy SkyCoord of length 3 containing the coordinates
+  
+  '''
   ccoords = coords.cartesian
   A = ccoords[1] - ccoords[0]
   B = ccoords[2] - ccoords[1]
@@ -104,6 +132,11 @@ def calc_circumcentre(coords):
 def get_centre(members, coords, seps):
   '''
   Calculate the centre of the smallest circle that encloses every target in the cluster. It is either the midpoint of the two furthest apart targets, or the circumcentre of the triangle made up of those two targets and the target furthest away from that midpoint.
+  
+  :param members: a list or numpy array containing the members of the cluster
+  :param coords: an astropy SkyCoord containing the coordinates of every target
+  :param seps: a numpy array containing the separation between each combination of targets
+  
   '''
   
   # Get the separation between each combination of members
@@ -129,7 +162,60 @@ def get_centre(members, coords, seps):
   return centre
     
   
+def do_clustering(coords, all_clusters, seps):
+  '''
   
+  :param coords: an astropy SkyCoord containing the coordinates of every target
+  :param all_clusters: a
+  :param seps: a numpy array containing the separation between each combination of targets
+  
+  '''
+  assigned = []
+  clusters = []
+  clustering = np.asarray(range(len(coords)))
+
+  centroids = {}
+  int_time = np.ones(shape=len(coords))
+  cluster_info = {}
+
+  for c_num in sorted(all_clusters.keys(), reverse=True):
+    members = np.asarray(all_clusters[c_num],dtype=int)
+
+    if len(list(set(members).intersection(assigned))) > 0:
+      continue
+    
+    num_targets = len(members)
+    centre = get_centre(members, coords, seps)
+    
+    coords_members = coords[members]  
+    
+    break_even_dist = get_rad(num_targets**-0.5)
+    
+    dist_from_centre = np.max(centre.separation(coords_members)).degree
+    
+   
+    
+    if dist_from_centre < break_even_dist:
+      assigned.extend(members)
+      clusters.append(c_num)
+        
+      centroids[c_num] = centre
+      
+      clustering[members] = c_num
+      
+      P = beam_fit(dist_from_centre*60)
+      
+      rel_int_time = P**-2/num_targets
+      int_time[members] = rel_int_time
+      
+      cluster_info[c_num] = {'members':members, 'centroid': centre, 'int_time': rel_int_time}
+    
+  
+  for c_num in range(len(coords)):
+    if c_num not in assigned:
+      cluster_info[c_num] = {'members':[c_num], 'centroid': coords[members], 'int_time': 1.0}
+  
+  return cluster_info
   
   
 
@@ -144,90 +230,27 @@ linked = linkage(dist, method='complete', optimal_ordering=True)
 
 
 all_clusters = extract_levels(linked, labellist)
-assigned = []
-clusters = []
-clustering = np.asarray(range(len(coords)))
 
-centroids = {}
-int_time = np.ones(shape=len(coords))
-
-for c_num in sorted(all_clusters.keys(), reverse=True):
-  members = np.asarray(all_clusters[c_num],dtype=int)
-
-  if len(list(set(members).intersection(assigned))) > 0:
-    continue
-  
-  num_targets = len(members)
-  centre = get_centre(members, coords, seps)
-  
-  coords_members = coords[members]  
-  
-  break_even_dist = get_rad(num_targets**-0.5)
-  
-  dist_from_centre = np.max(centre.separation(coords_members)).degree
-  
- 
-  
-  if dist_from_centre < break_even_dist:
-    assigned.extend(members)
-    clusters.append(c_num)
-      
-    centroids[c_num] = centre
-    
-    clustering[members] = c_num
-    
-    P = beam_fit(dist_from_centre*60)
-    
-    int_time[members] = P**-2/num_targets
-print(sum(int_time)/len(int_time))
+cluster_info = do_clustering(coords, all_clusters, seps)
 
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-for i in range(max(clustering)):
-  indices = np.where(clustering == i)[0]
+for c_num in cluster_info.keys():
+  cluster_data = cluster_info[c_num]
+  indices = cluster_data['members']
+  
   num_coords = len(indices)
-  if num_coords < 1:
-    continue
   ccoords = coords[indices]
   
   ax.scatter(ccoords.ra, ccoords.dec)
   
   if num_coords > 1:
-    centroid = centroids[i]
+    centroid = cluster_data['centroid']
     ax.plot(centroid.ra, centroid.dec, c='k', marker='x')
     circ = plt.Circle((centroid.ra.value, centroid.dec.value), get_rad(num_coords**-0.5), color='k', alpha=0.5)
     ax.add_artist(circ)
-  
-  
-plt.show()
 
-exit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-dendrogram(linked,  
-            orientation='top',
-            labels=labellist,
-            distance_sort='descending',
-            show_leaf_counts=True)
-plt.figure()         
-dendrogram(linked_adj,  
-            orientation='top',
-            labels=labellist,
-            distance_sort='descending',
-            show_leaf_counts=True)
-            
 plt.show()
